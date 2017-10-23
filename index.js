2e7f51489a4264dc704439d6ef07d341079bd3d1
Daniel Olivera <dannyolivera11@gmail.com>
2017-10-23 03:21:05 -0700

    added get row and add row general functionality

--- index.js
+++ index.js
@@ -1,49 +1,58 @@
 //const QUERY = require('./query');
 const SCHEDULE = require("node-schedule");
 const MONGOOSE = require('mongoose');
-const GoogleSpreadsheet = require("google-spreadsheet");
-const Credentials = require("./secret.json");
+var GoogleSpreadsheet = require("google-spreadsheet");
+const GOOGLE = require("./secret.json");
+const SHEETS = require("./sheets.json");
+const dbPath = 'mongodb://localhost/userDB';
+var async = require('async');
 
-var dbPath = 'mongodb://localhost/userDB';
 
 
-var createSpreadsheet = function() {
-  var gDoc = new GoogleSpreadsheet('ADD LONG ID FROM SHEETS URL');
-  var gSheet = "";
-}
+// spreadsheet key is the long id in the sheets URL
+var doc = new GoogleSpreadsheet(SHEETS.sheet_id);
+var sheet;
 
-var query = function () {
-
-    connectDB();
-    findAll();
-    setTimeout( dissconnectDB, 3000);
-}
-
-var dissconnectDB = function () {
-    MONGOOSE.disconnect();
-    console.log("disconnect");
-}
-
-var createWorkBook = function () {
-    XLSX.writeFile(workbook, 'tracking.xlsx');
-}
-
-var connectDB = function () {
-    var db = MONGOOSE.connect(dbPath, { useMongoClient: true });
-    db.on('error', console.error.bind(console, 'connection error: '));
-    db.once('open', function () {
-        console.log("success")
+async.series([
+  function setAuth(callback) {
+    doc.useServiceAccountAuth(GOOGLE, callback);
+  },
+  function getInfoAndWorksheets(callback) {
+    doc.getInfo(function (err, info) {
+        console.log('Loaded doc: ' + info.title + ' by ' + info.author.email);
+        sheet = info.worksheets[0];
+        console.log('sheet 1: ' + sheet.title + ' ' + sheet.rowCount + 'x' + sheet.colCount);
+        callback();
     });
-}
+  },
+  function getRows(callback) {
+    sheet.getRows({
+      offset: 1
+    }, function(err, rows){
+      var length =rows.length-1;
+      var date = new Date();
+      rows[length].date = date.toISOString().substring(0,10);
+      rows[length].save();
 
-var findAll = function () {
+      callback();
 
-    USER.find({}, function (err, users) {
-        if (!err) {
-            console.log(users);
-        } else { throw err; }
     });
-
-}
-
-var sched = SCHEDULE.scheduleJob('*/1 * * * *', query)
+  },
+  function appendRows(callback) {
+    var date = new Date().toISOString().substring(0,10);
+    sheet.addRow({
+      date: date,
+      tasks_created_today: "1",
+      tasks_completed_today: "2",
+      number_of_users: "3"
+    }, function(err){
+      ///
+      callback();
+    });
+  }
+],
+function (err) {
+  if (err) {
+    console.log('Error: ' + err);
+  }
+});
